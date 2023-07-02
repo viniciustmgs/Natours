c32abda37cb9aca8af3e583f0260aee7a5e27df5
viniciustmgs <viniciustmgs@gmail.com>
2023-07-02 16:19:53 -0300

    configured mongoDB initial connection

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DB_CONNECTION.replace(
+  '<PASSWORD>',
+  process.env.DB_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connection successful'));
+
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
