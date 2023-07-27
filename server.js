c7e407997d5a4680d5451e8aece388bcd72be06e
viniciustmgs <viniciustmgs@gmail.com>
2023-07-27 09:56:16 -0300

    removing env

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+//const dotenv = require('dotenv');
 
 process.on('uncaughtException', (err) => {
   console.log('UNCAUGHT EXCEPTION! Shutting down...');
@@ -7,7 +7,7 @@ process.on('uncaughtException', (err) => {
   process.exit(1);
 });
 
-dotenv.config({ path: './.env' });
+//dotenv.config({ path: './.env' });
 
 const app = require('./app');
 
